51fbf6800223ec2fae18669394dd0c11f817e5f0
vipulswarup <vipul.swarup@eisenvault.com>
2015-08-04 22:00:09 +0530

    Updated alfresco root location

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ cp pdf2pdf.sh /opt/ocr
 chmod a+x /opt/ocr/convert2pdf.sh
 
 #step 2 - stop alfresco
-/opt/alfresco-5.0.d/alfresco.sh stop
+/opt/alfresco/alfresco.sh stop
 
 #step 3 - restore share.war and alfresco.war to OOB versions
 cd share_oob
@@ -19,26 +19,26 @@ zip -r ../share_oob.zip .
 cd ../alfresco_oob
 zip -r ../alfresco_oob.zip .
 cd ..
-cp share_oob.zip /opt/alfresco-5.0.d/tomcat/webapps/share.war
-cp alfresco_oob.zip /opt/alfresco-5.0.d/tomcat/webapps/alfresco.war
+cp share_oob.zip /opt/alfresco/tomcat/webapps/share.war
+cp alfresco_oob.zip /opt/alfresco/tomcat/webapps/alfresco.war
 
 rm share_oob.zip
 rm alfresco_oob.zip
 
 #step 4 copy and apply AMP file with Alfresco code changes
 #to be done manually
-cp evThemes_eisenVaultTheme.amp /opt/alfresco-5.0.d/amps_share/
-cp share-amp.amp /opt/alfresco-5.0.d/amps_share/
-cp repo-amp.amp /opt/alfresco-5.0.d/amps/
+cp evThemes_eisenVaultTheme.amp /opt/alfresco/amps_share/
+cp share-amp.amp /opt/alfresco/amps_share/
+cp repo-amp.amp /opt/alfresco/amps/
 /opt/alfresco-5.0.d/bin/apply_amps.sh -force
 
-cp ev_share_header_modules.jar /opt/alfresco-5.0.d/tomcat/shared/lib
+cp ev_share_header_modules.jar /opt/alfresco/tomcat/shared/lib
 
 #step 5 make a temp directory
 mkdir /tmp/Alfresco
 
 #step 6 - remove old alfresco, solr4 and share log files
-rm /opt/alfresco-5.0.d/*.log
+rm /opt/alfresco/*.log
 
 #step 7 - stop alfresco
-/opt/alfresco-5.0.d/alfresco.sh start
+/opt/alfresco/alfresco.sh start
