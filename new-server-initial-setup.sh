c8ab860c320a40e9245d64e304c805710d79ef82
Vipul Swarup <vipul.swarup@eisenvault.com>
2015-09-24 21:33:05 +0530

    Update new-server-initial-setup.sh

--- new-server-initial-setup.sh
+++ new-server-initial-setup.sh
@@ -2,3 +2,6 @@ apt-get update
 apt-get upgrade
 apt-get install ghostscript imagemagick libjpeg62 libgif4 libart-2.0-2 libreoffice
 sudo dpkg-reconfigure tzdata
+
+update-locale LANG="en_US"
+update-locale LC_CTYPE=UTF-8
