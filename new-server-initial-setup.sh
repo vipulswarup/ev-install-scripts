f700defc8464257d37207c8e46489f2bd03c30ed
Vipul Swarup <vipul.swarup@eisenvault.com>
2015-11-24 16:41:26 +0530

    Changed language to en_US

--- new-server-initial-setup.sh
+++ new-server-initial-setup.sh
@@ -1,11 +1,11 @@
 apt-get update
 apt-get upgrade
 apt-get install ghostscript imagemagick libjpeg62 libgif4 libart-2.0-2 libreoffice
-sudo dpkg-reconfigure tzdata
+dpkg-reconfigure tzdata
 
 #set locale = Vipul 19 Aug 2015
-export LANGUAGE=en_IN.UTF-8
-export LANG=en_IN.UTF-8
-export LC_ALL=en_IN.UTF-8
-locale-gen en_IN.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
 dpkg-reconfigure locales
